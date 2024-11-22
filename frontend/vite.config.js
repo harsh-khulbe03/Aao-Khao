b495f3e41a1127b483c014f39aecaf4ec49a4c55
harsh-khulbe03 <harshkhulbe18@gmail.com>
2024-11-22 10:04:42 +0000

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import { defineConfig } from 'vite';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     mimeTypes: {
+//       '.js': 'application/javascript',
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeTypes: {
-      '.js': 'application/javascript',
-    },
-  },
-});
+})
